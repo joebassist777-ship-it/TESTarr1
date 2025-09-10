264cc03234905620893ad8e76fbb1f515abe3525
joebassist777-ship-it <joebassist777@gmail.com>
2025-09-10 16:35:31 +0900

    Update Lotto1.java

--- Lotto1.java
+++ Lotto1.java
@@ -5,41 +5,28 @@ public class Ball implements Comparable<Ball> {
     private int no; // 공의 번호
 
     // 생성자: 공 번호를 받아서 초기화
-    public Ball(int no) {
-        this.no = no;
-    }
+    public Ball(int no) {this.no = no;}
 
     // 공 번호를 반환하는 메서드
-    public int getNo() {
-        return this.no;
-    }
+    public int getNo() {return this.no;}
 
     // 공 비교: 번호 기준으로 정렬하기 위해 사용
     @Override
     public int compareTo(Ball o) {
-        if (this.no > o.no) {
-            return 1;
-        } else if (this.no < o.no) {
-            return -1;
-        }
-        return 0;
-    }
+        if (this.no > o.no) {return 1;
+        } 
+        else if (this.no < o.no) {return -1;}
+            return 0;}
 
     // 공을 문자열로 출력할 때 번호만 보여줌
     @Override
-    public String toString() {
-        return String.valueOf(this.no);
-    }
+    public String toString() {return String.valueOf(this.no);}
 
     // equals와 hashCode는 기본 Object 방식 사용 (중복 체크에 사용됨)
-    public boolean equals(Object obj) {
-        return super.equals(obj);
-    }
+    public boolean equals(Object obj) {return super.equals(obj);}
 
     @Override
-    public int hashCode() {
-        return super.hashCode();
-    }
+    public int hashCode() {return super.hashCode();}
 }
 
 =============================================
@@ -56,13 +43,10 @@ public class LottPa {
     private Set<Ball> lottoSet; // 사용자가 입력한 번호를 저장할 Set
 
     // 생성자: TreeSet으로 초기화 (중복 제거 + 자동 정렬)
-    public LottPa() {
-        lottoSet = new TreeSet<Ball>();
-    }
+    public LottPa() {lottoSet = new TreeSet<Ball>();}
 
     // 사용자에게 번호를 입력받는 메서드
-    public void write() {
-        Scanner scanner = new Scanner(System.in);
+    public void write() {Scanner scanner = new Scanner(System.in);
         System.out.println("=======================");
 
         // 6개의 번호를 입력받을 때까지 반복
@@ -74,15 +58,11 @@ public class LottPa {
                 break;
             }
         }
-
         scanner.close(); // 입력 종료
         System.out.println(); // 줄 바꿈
     }
-
     // 사용자가 입력한 번호들을 반환
-    public Set<Ball> getLottoset() {
-        return lottoSet;
-    }
+    public Set<Ball> getLottoset() {return lottoSet;}
 }
 =====================================
 
@@ -99,9 +79,7 @@ public class LottoMac {
     private Set<Ball> lottoSet; // 컴퓨터가 생성한 번호 저장
 
     // 생성자: TreeSet으로 초기화
-    public LottoMac() {
-        lottoSet = new TreeSet<Ball>();
-    }
+    public LottoMac() {lottoSet = new TreeSet<Ball>();}
 
     // 랜덤으로 1~45 사이의 번호 6개 생성
     public void initLotto() {
@@ -149,9 +127,7 @@ public class LottoMac {
     }
 
     // 컴퓨터 번호를 외부에서 가져올 수 있도록 getter 제공
-    public Set<Ball> getLottoSet() {
-        return lottoSet;
-    }
+    public Set<Ball> getLottoSet() {return lottoSet;}
 }
 ==============================================
 package lotto1;
